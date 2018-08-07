fc7da6f5a52dc44b5fd97f1680060aa4d21db3b4
Mac Heller-Ogden <mac@machellerogden.com>
2018-08-07 08:37:12 -0500

    minor fix to set primitives instead of converting to array and corresponding tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 const chai = require('chai');
 const expect = chai.expect;
-const { merge } = require('..');
+const { merge, set } = require('..');
 
 describe('merge', () => {
     it('[+] syntax should push to array', () => {
@@ -159,6 +159,180 @@ describe('merge', () => {
                 'bar[11,0]': { l: 'l' }
             }
         })).to.eql({
+            foo: {
+                bar: [
+                    {
+                        a: 'a'
+                    },
+                    {
+                        b: 'b'
+                    },
+                    {
+                        c: 'c'
+                    },
+                    {
+                        d: 'd'
+                    },
+                    {
+                        e: 'e'
+                    },
+                    {
+                        f: 'f'
+                    },
+                    {
+                        g: 'g'
+                    },
+                    {
+                        h: 'h'
+                    },
+                    {
+                        i: 'i'
+                    },
+                    {
+                        j: 'j'
+                    },
+                    {
+                        k: 'k'
+                    },
+                    {
+                        l: 'l'
+                    },
+                    {
+                        m: 'm'
+                    },
+                    {
+                        n: 'n'
+                    }
+                ]
+            }
+        });
+
+    });
+});
+
+describe('set', () => {
+
+    it('when no special syntax in keypath and when operating on a primitive value should replace the value', () => {
+        expect(set({
+            foo: {
+                bar: 'bar'
+            }
+        }, 'foo.bar', 'bah')).to.eql({
+            foo: {
+                bar: 'bah'
+            }
+        });
+    });
+
+    it('[-] syntax should unshift to array', () => {
+
+        expect(set({
+            foo: {
+                bar: [
+                    {
+                        baz: 'sis'
+                    },
+                    {
+                        qux: 'boom'
+                    }
+                ]
+            }
+        }, 'foo.bar[-]', { xyzzy: 'bah' })).to.eql({
+            foo: {
+                bar: [
+                    {
+                        xyzzy: 'bah'
+                    },
+                    {
+                        baz: 'sis'
+                    },
+                    {
+                        qux: 'boom'
+                    }
+                ]
+            }
+        });
+
+    });
+
+    it('[<int>,<int>] syntax should splice to array', () => {
+
+        expect(set({
+            foo: {
+                bar: [
+                    {
+                        baz: 'sis'
+                    },
+                    {
+                        qux: 'boom'
+                    }
+                ]
+            }
+        }, 'foo.bar[1,0]', { xyzzy: 'bah' })).to.eql({
+            foo: {
+                bar: [
+                    {
+                        baz: 'sis'
+                    },
+                    {
+                        xyzzy: 'bah'
+                    },
+                    {
+                        qux: 'boom'
+                    }
+                ]
+            }
+        });
+
+    });
+
+    it('[<int>,<int>] syntax should splice even with large indexes', () => {
+
+        expect(set({
+            foo: {
+                bar: [
+                    {
+                        a: 'a'
+                    },
+                    {
+                        b: 'b'
+                    },
+                    {
+                        c: 'c'
+                    },
+                    {
+                        d: 'd'
+                    },
+                    {
+                        e: 'e'
+                    },
+                    {
+                        f: 'f'
+                    },
+                    {
+                        g: 'g'
+                    },
+                    {
+                        h: 'h'
+                    },
+                    {
+                        i: 'i'
+                    },
+                    {
+                        j: 'j'
+                    },
+                    {
+                        k: 'k'
+                    },
+                    {
+                        m: 'm'
+                    },
+                    {
+                        n: 'n'
+                    }
+                ]
+            }
+        }, 'foo.bar[11,0]', { l: 'l' })).to.eql({
             foo: {
                 bar: [
                     {
