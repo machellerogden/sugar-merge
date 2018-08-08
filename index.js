48b355ba900a6e8d404e1ec7e5adedd2f9c5a000
Mac Heller-Ogden <mac@machellerogden.com>
2018-08-07 21:40:45 -0500

    critical fix

--- index.js
+++ index.js
@@ -3,14 +3,15 @@
 const deepmerge = require('./lib/deepmerge');
 const process = require('./lib/process');
 const expand = require('./lib/expand');
+const merge = (acc, ...args) => process(deepmerge(acc, ...args));
+const set = (obj, keypath, value) => process(expand(obj, keypath, value));
 
 const sugar = {
     expand,
     process,
     deepmerge,
-    merge: (acc, ...args) => process(deepmerge(acc, ...args)),
-    set: (obj, keypath, value) => process(expand(obj, keypath, value))
+    merge,
+    set
 };
-
 
 module.exports = sugar;
