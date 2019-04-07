4c973f4d359b6a37db6f6e166311d6bc15bcc3ac
Mac Heller-Ogden <mac@machellerogden.com>
2019-04-06 22:11:47 -0500

    use needful

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const deepmerge = require('./lib/deepmerge');
+const { merge:deepmerge } = require('needful');
 const process = require('./lib/process');
 const expand = require('./lib/expand');
 const merge = (acc, ...args) => process(deepmerge(acc, ...args));
@@ -9,7 +9,6 @@ const set = (obj, keypath, value) => process(expand(obj, keypath, value));
 const sugar = {
     expand,
     process,
-    deepmerge,
     merge,
     set
 };
